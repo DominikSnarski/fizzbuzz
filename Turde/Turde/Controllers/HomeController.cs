19b94292b7112c18f7caf23274d7f11d40f6d547
DominikSnarski <39265236+DominikSnarski@users.noreply.github.com>
2019-03-11 22:34:57 +0100

    Add files via upload

---
+++ Turde/Turde/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Turde.Models;
+
+namespace Turde.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int x)
+        {
+            string output;
+            if(x<1 || x>100)
+            {
+                output = "Please submit integer between 1 and 100.";
+            }
+            else if (x % 3 == 0 && x % 5 == 0)
+            {
+                output = "fizzbuzz";
+            }
+            else if (x % 3 == 0)
+            {
+                output = "fizz";
+            }
+            else if (x % 5 == 0)
+            {
+                output = "buzz";
+            }
+            else
+            {
+                output = x.ToString();
+            }
+            ViewBag.Mydata = output;
+            return View();
+        }
+
+    }
+}
---
+++ Turde/Turde/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Turde.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
